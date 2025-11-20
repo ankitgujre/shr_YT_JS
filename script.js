b6342f145de8cfc15da1b48e1dd724b2d35cb5f6
ankitgujre <ankitgujre374@gmail.com>
2025-11-20 16:43:29 +0530

    Var

---
+++ script.js
@@ -0,0 +1,18 @@
+// word vs keyword
+
+/**Ram shyam ankit */
+/** for let var const = keyword */
+
+/**-----------------------------Variables and Decalarations---------------------------- */
+
+// var let const - line by line comparision
+var a = 12;    // globally use kr sakte hai
+console.log(a)
+
+// decalaration and initialization
+
+var a; // yeh hota hai declare karna
+var b = 15;  // yeh hota hai declare and initialize karna
+
+// initialize ka matlab hota hai 1st value dena
+
